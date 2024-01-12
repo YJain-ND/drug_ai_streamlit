9e41104d600fad099c35bbe6a19cafb2ea129580
YJain-ND <yash.jain@neodocs.in>
2024-01-12 17:53:39 +0530

    formatting

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import random
 from main import medicine_details,medication_optimization
 
-st.title("MedBot")
+st.title("DrugAI")
+st.markdown(f"""<p style="font-size:1.5rem;">Type your medication in the text box bellow and get to know more about your medication.</p>""",unsafe_allow_html=True)
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -25,10 +26,17 @@ if prompt := st.chat_input("Enter your Medication (comma seperated)"):
     response = f"Collecting information for {prompt.title()} ....."
     # st.chat_message("assistant").write(response)
     # medicine,salts,full_match,other_matches = medicine_details(prompt.lower())
-    final_med,total_salts,salts,medicines = medication_optimization(all_meds)
+    # final_med,total_salts,salts,medicines = medication_optimization(all_meds)
+    enter = "\n\n"
+    with st.spinner("Collecting Information for your medication..."):
+        (
+            final_med,
+            total_salts,
+            salts,
+            medicines,
+        ) = medication_optimization(all_meds)
     print("Total salts",total_salts)
     print("Salts",salts)
-    enter = "\n\n"
     response = f"**For the following medicines:**  {', '.join([s.title() for s in medicines])}.\n\n :red[**You are consuming:**] {', '.join([s.title() for s in salts])}"
     with st.chat_message("assistant"):
         st.markdown(response)
--- temp1.py
+++ temp1.py
@@ -1,34 +1,7 @@
-from itertools import chain, combinations
-
-def get_subsets(lst):
-    return list(chain.from_iterable(combinations(lst, r) for r in range(1,len(lst) + 1)))
-
-def find_combinations(subsets):
-    full_set = set().union(*subsets)
-    
-    def dfs(index, current_combination, remaining_elements):
-        if not remaining_elements:
-            combinations.append(current_combination)
-            return
-
-        for i in range(len(subsets)-1,index-1, -1):
-            new_combination = current_combination + [subsets[i]]
-            new_remaining = remaining_elements - set(subsets[i])
-            dfs(i + 1, new_combination, new_remaining)
-
-    combinations = []
-    dfs(0, [], full_set)
-
-    return combinations
-
-# Example usage
-my_list = ["a", "b", "c"]
-subsets = get_subsets(my_list)
-
-combinations = find_combinations(subsets)
-
-# Print the result
-for combination in combinations:
-    print(list(combination))
-
-print(len(combinations))
+with st.spinner("Thinking..."):
+        (
+            full_response,
+            total_tokens,
+            prompt_tokens,
+            completion_tokens,
+        ) = generate_response(prompt)
